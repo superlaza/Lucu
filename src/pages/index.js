ae3b2d8f17eba1628a7fb4ba7c79cc35dc868004
David Perlaza <superlaza7@gmail.com>
2021-04-27 13:52:50 -0400

    service images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,13 @@ import '../styles/main.scss';
 
 import Layout from '../components/layout';
 import Header from '../components/header';
-import Logo from '../components/_common/logo';
+
+import MarketingIcon from '../images/service/marketing.svg'
+import ConnectIcon from '../images/service/connect.svg'
+import PatternsIcon from '../images/service/patterns.svg'
+import InsightIcon from '../images/service/insight.svg'
+import GearsIcon from '../images/service/gears.svg'
+
 
 // markup
 const IndexPage = () => {
@@ -12,7 +18,32 @@ const IndexPage = () => {
     <Layout>
       <main>
         <title>Home Page</title>
-        <Logo/>
+
+        <div>
+          <img class="service-icon" src={MarketingIcon}/>
+          <span class="service-header">Marketing</span>
+        </div>
+
+        <div>
+          <img class="service-icon" src={ConnectIcon}/>
+          <span class="service-header">Connect with your customers</span>
+        </div>
+
+        <div>
+          <img class="service-icon" src={PatternsIcon}/>
+          <span class="service-header">Discover patterns</span>
+        </div>
+
+        <div>
+          <img class="service-icon" src={InsightIcon}/>
+          <span class="service-header">Gain insights</span>
+        </div>
+
+        <div>
+          <img class="service-icon" src={GearsIcon}/>
+          <span class="service-header">Automate</span>
+        </div>
+
       </main>
     </Layout>
   )
